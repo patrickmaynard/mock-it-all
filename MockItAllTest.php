412f7490cd12f95c5a56b96b69a6758119d15738
Patrick Maynard <patrickmaynard.bs@gmail.com>
2026-01-15 15:35:35 +0100

    Update MockItAllTest.php

--- MockItAllTest.php
+++ MockItAllTest.php
@@ -2,157 +2,50 @@
 
 namespace App\Tests\Unit\Service\Codict;
 
-use App\Factory\Mepwall\OrganDescriptionFactory;
-use App\Factory\Mepwall\OrganFactory;
-use App\Repository\StationMepwallOrganeDescRepository;
-use App\Repository\StationMepwallOrganeRepository;
-use App\Service\Codict\Fetcher;
-use App\Service\Codict\OrganSyncer;
-use Doctrine\ORM\EntityManagerInterface;
-use Psr\Log\NullLogger;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
-use Symfony\Component\Serializer\SerializerInterface;
+use App\Service\Codict\CodictService;
 
-class OrganSyncerTest extends KernelTestCase
+class MockItAllTest extends KernelTestCase
 {
-    /**
-     * @param string[] $organIds
-     *
-     * @dataProvider provideSync
-     *
-     * @throws \Exception
-     */
-    public function testSync(
-        array $organIds,
-        int $validOrgansCount,
-        int $validDescCount,
-    ): void {
-        self::bootKernel();
-
-        $container = static::getContainer();
-
-        $mepwallOrganeRepo = $this->createMock(StationMepwallOrganeRepository::class);
-        $mepwallOrganeRepo->expects(self::once())->method('deleteAll');
-
-        $mepwallOrganeDescRepo = $this->createMock(StationMepwallOrganeDescRepository::class);
-        $mepwallOrganeDescRepo->expects(self::once())->method('deleteAll');
-
-        $fetcher = $this->createMock(Fetcher::class);
-        $fetcher->expects(self::once())->method('fetchAllOrganeIDs')->willReturn($organIds);
-        $fetcher
-            ->expects(self::exactly(count($organIds)))
-            ->method('fetchOrganeDataByID')
-            ->willReturnCallback(function ($organID) {
-                return match ($organID) {
-                    '1' => [],
-                    '2' => $this->createOrganData(2, 0),
-                    '3' => $this->createOrganData(1, 1),
-                    '4' => $this->createOrganData(0, 2),
-                };
-            });
-
-        $organFactory = $this->createMock(OrganFactory::class);
-        $organFactory->expects(self::exactly($validOrgansCount))->method('createFromCodictDto');
-
-        $organDescFactory = $this->createMock(OrganDescriptionFactory::class);
-        $organDescFactory->expects(self::exactly($validDescCount))->method('createFromCodictDto');
-
-        $em = $this->createMock(EntityManagerInterface::class);
-        $em->expects(self::exactly($validOrgansCount + $validDescCount))->method('persist');
-
-        $subject = new OrganSyncer(
-            $mepwallOrganeRepo,
-            $mepwallOrganeDescRepo,
-            $organFactory,
-            $organDescFactory,
-            $fetcher,
-            $container->get('serializer'),
-            $em,
-        );
-
-        $subject->sync(new NullLogger());
-    }
-
-    private function createOrganData(int $validDesc, int $invalidDesc): array
+    public function setUp(): void
     {
-        $organData = [
-            'bodyId' => 2,
-            'bodyType' => 'A',
-            'activatedDate' => 'A',
-            'updateDate' => 'A',
-            'deletedDate' => 'A',
-        ];
+        //This is where we will set up our mock objects.
+        //We want to do this in the most generic way possible.
+        //First, we will define an array of mock relationships.
+        //(These will eventually be object, but nested arrays are fine for now.)
+        $mockRelationships = [];
+        $topLevelClassNameToMock = CodictService::class;
 
-        for ($i = 0; $i < $validDesc; ++$i) {
-            $organData['descriptions'][] = [
-                'langIsoCode' => 'ss',
-                'fullName' => 'ss',
-                'shortName' => 'ss',
-                'order' => 3,
-            ];
-        }
+        //Each nested mockRelationship we create will be an array of the form:
+        //[
+        //    'classNameToMock' => <the class name to mock as a string>,
+        //    'children' => <more mockRelationships go here as an array>,
+        //    'mockCreationCommand' => <the mock creation command as a string>,
+        //]
 
-        for ($i = 0; $i < $invalidDesc; ++$i) {
-            $organData['descriptions'][] = [
-                'langIsoCode' => '',
-                'fullName' => 'ss',
-                'shortName' => 'ss',
-                'order' => 3,
-            ];
-        }
+        //There will be three steps to this process.
+        //First, we will run $this->createMockRelationships($topLevelClassNameToMock, $mockRelationships).
+        //That command will recursively call itself until we have a nice array of mock relationships.
+        //However, the 'mockCreationCommand' values will be placeholder strings like '---' for now.
+        //Then we will run $this->populateMockCreationCommands($mockRelationships).
+        //That command will replace the 'mockCreationCommand' values with actual mock creation commands.
+        //Finally, we will run $this->createFinalMockCreationLogic($mockRelationships).
+        //That command will run recursively in order to assemble the order of operations for mock creation.
+        //It is very important that the longest chain of dependencies run first,
+        //and that it start at the dependency farthest from the top level class.
+        //In other words, we assemble the little constituant pieces first, \
+        //allowing us to later assemble the whole thing.
 
-        return $organData;
+        //$relationships = $this->createMockRelationships($topLevelClassNameToMock, $mockRelationships);
+        //$relationships = $this->populateMockCreationCommands($relationships);
+        //$finalLogic    = $this->createFinalMockCreationLogic($relationships);
+
     }
 
-    public function provideSync(): array
+    public function testTheTruthyThing(): void
     {
-        return [
-            'All organs have data' => [
-                ['2', '2'],
-                2,
-                4,
-            ],
-            'Some organs do not have data' => [
-                ['1', '2'],
-                1,
-                2,
-            ],
-            'Some organs do not have valid description' => [
-                ['1', '2', '3', '4'],
-                3,
-                3,
-            ],
-            'All organs have no data' => [
-                ['1', '1'],
-                0,
-                0,
-            ],
-        ];
+        self::assertTrue(true);
     }
 
-    public function testSyncException(): void
-    {
-        $this->expectException(\Exception::class);
 
-        $mepwallOrganeRepo = $this->createMock(StationMepwallOrganeRepository::class);
-        $mepwallOrganeRepo->expects(self::once())->method('deleteAll');
-
-        $mepwallOrganeDescRepo = $this->createMock(StationMepwallOrganeDescRepository::class);
-        $mepwallOrganeDescRepo->expects(self::once())->method('deleteAll');
-
-        $fetcher = $this->createMock(Fetcher::class);
-        $fetcher->expects(self::once())->method('fetchAllOrganeIDs')->willReturn([]);
-
-        $subject = new OrganSyncer(
-            $mepwallOrganeRepo,
-            $mepwallOrganeDescRepo,
-            $this->createMock(OrganFactory::class),
-            $this->createMock(OrganDescriptionFactory::class),
-            $fetcher,
-            $this->createMock(SerializerInterface::class),
-            $this->createMock(EntityManagerInterface::class),
-        );
-
-        $subject->sync(new NullLogger());
-    }
 }
